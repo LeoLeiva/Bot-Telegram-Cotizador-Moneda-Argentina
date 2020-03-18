9e7215ba66ba2bcd7d767ec4b3e8ac474b3af87a
Leo Leiva <leocableiva@gmail.com>
2020-03-17 22:57:46 -0300

    Se modifico el codigo para que la seleccion sea con botones y se separo el token pasandolo a otro archivo

---
+++ clave.py
@@ -0,0 +1,1 @@
+idtoken = "1110773023:AAHuxnjeO_XzAc-n_6JZtSRDn4W7FtmoAZ8"
--- cotizadorbot.py
+++ cotizadorbot.py
@@ -2,31 +2,90 @@
 # -*- coding: utf-8 -*-
 import logging
 import requests
+from clave import idtoken
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-
-# Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
-
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
-    """Send a message when the command /start is issued."""
-    update.message.reply_text("Hola! Por favor selecciona la moneda")
-    update.message.reply_text("Dolar oficial /dolar")
-    update.message.reply_text("Dolar blue /dolarblue")
-    update.message.reply_text("Euro oficial /euro")
-    update.message.reply_text("Euro blue /euroblue")
-    update.message.reply_text("Para volver a ver este menu simplemente toca en /start")
+    keyboard = [[InlineKeyboardButton("Dolar", callback_data="dolar"),
+                 InlineKeyboardButton("Dolar Blue", callback_data="dolarblue")],
+
+                [InlineKeyboardButton("Euro", callback_data="euro"),
+                InlineKeyboardButton("Euro Blue", callback_data="euroblue")]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Que cotizacion deseas:', reply_markup=reply_markup, parse_mode='HTML',)
+
+
+def button(update, context):
+    query = update.callback_query
+
+    r = requests.get('http://api.bluelytics.com.ar/v2/latest').json()
+
+    #Rescatando datos del dolar oficial
+    oficompra = float(r["oficial"]['value_buy'])
+    coficompra = '%.2f' %oficompra
+    ofiventa = float(r["oficial"]['value_sell'])
+    cofiventa = '%.2f' %ofiventa
+    soli = float(ofiventa + (ofiventa*0.3))
+    csoli = '%.2f' %soli
+    
+    #rescatando datos del dolar blue
+    bluecompra = float(r["blue"]['value_buy'])
+    cbluecompra = '%.2f' %bluecompra
+    blueventa = float(r["blue"]['value_sell'])
+    cblueventa= '%.2f' %blueventa
+    
+    #Rescatando datos del euro
+    eoficompra = float(r["oficial_euro"]['value_buy'])
+    ceoficompra = '%.2f' %eoficompra
+    eofiventa = float(r["oficial_euro"]['value_sell'])
+    ceofiventa = '%.2f' %eofiventa
+    esoli = float(eofiventa + (eofiventa*0.3))
+    cesoli = '%.2f' %esoli
+    
+    #Rescatando datos del euro blue
+    ebluecompra = float(r["blue_euro"]['value_buy'])
+    cebluecompra = '%.2f' %ebluecompra
+    eblueventa = float(r["blue_euro"]['value_sell'])
+    ceblueventa = '%.2f' %eblueventa
+    timeupdate = str(r["last_update"])
+
+
+    dolar = "U$S Oficial Compra      | "  + str(coficompra) + "\n\n"
+    dolar += "U$S Oficial Venta         | " + str(cofiventa) + "\n\n"
+    dolar += "U$S Oficial C/Imp. Pais | " + str(csoli) + "\n\n"
+
+    dolarblue = "U$S Blue Compra      | "  + str(cbluecompra) + "\n\n"
+    dolarblue += "U$S Blue Venta         | " + str(cblueventa) + "\n\n"
+
+    euro = "Euro Oficial Compra      | "  + str(ceoficompra) + "\n\n"
+    euro += "Euro Oficial Venta         | " + str(ceofiventa) + "\n\n"
+    euro += "Euro Oficial C/Imp. Pais | " + str(cesoli) + "\n\n"
+    
+    euroblue = "Euro Blue Compra      | "  + str(cebluecompra) + "\n\n"
+    euroblue += "Euro Blue Venta         | " + str(ceblueventa) + "\n\n"
+
+    actualizacion = "Ultima Actualizacion: " + timeupdate[:10] + " | Hora: " + timeupdate[11:19]
+    
+    if query.data == "dolar":
+        query.edit_message_text(text=dolar + actualizacion + "\n\n Click en /start para elegir otra cotizacion")
+    elif query.data == "dolarblue":
+        query.edit_message_text(text=dolarblue + actualizacion + "\n\n Click en /start para elegir otra cotizacion")
+    elif query.data == "euro":
+        query.edit_message_text(text=euro + actualizacion + "\n\n Click en /start para elegir otra cotizacion")
+    elif query.data == "euroblue":
+        query.edit_message_text(text=euroblue + actualizacion + "\n\n Click en /start para elegir otra cotizacion")
 
 
 def help(update, context):
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text("Use /start para obtener las opciones de cotizacion")
 
 
 def error(update, context):
@@ -34,95 +93,22 @@ def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
-def dolar(update, content):
-    r = requests.get('http://api.bluelytics.com.ar/v2/latest').json()
-
-    oficompra = float(r["oficial"]['value_buy'])
-    coficompra = '%.2f' %oficompra
-    ofiventa = float(r["oficial"]['value_sell'])
-    cofiventa = '%.2f' %ofiventa
-    soli = float(ofiventa + (ofiventa*0.3))
-    csoli = '%.2f' %soli
-    timeupdate = str(r["last_update"])
-
-    update.message.reply_text("U$S Oficial Compra      | " + str(coficompra))
-    update.message.reply_text("U$S Oficial Venta       | " + str(cofiventa))
-    update.message.reply_text("U$S Oficial C/Imp. Pais | " + str(csoli))
-    update.message.reply_text("Ultima Actualizacion: " + timeupdate[:10] + " | Hora: " + timeupdate[11:19])
-
-
-def dolarblue(update, content):
-    r = requests.get('http://api.bluelytics.com.ar/v2/latest').json()
-
-    bluecompra = float(r["blue"]['value_buy'])
-    cbluecompra = '%.2f' %bluecompra
-    blueventa = float(r["blue"]['value_sell'])
-    cblueventa= '%.2f' %blueventa
-    timeupdate = str(r["last_update"])
-
-    update.message.reply_text("U$S Blue Compra      | " + str(cbluecompra))
-    update.message.reply_text("U$S Blue Venta       | " + str(cblueventa))
-    update.message.reply_text("Ultima Actualizacion: " + timeupdate[:10] + " | Hora: " + timeupdate[11:19])
-
-
-def euro(update, content):
-    r = requests.get('http://api.bluelytics.com.ar/v2/latest').json()
-
-    eoficompra = float(r["oficial_euro"]['value_buy'])
-    ceoficompra = '%.2f' %eoficompra
-    eofiventa = float(r["oficial_euro"]['value_sell'])
-    ceofiventa = '%.2f' %eofiventa
-    esoli = float(eofiventa + (eofiventa*0.3))
-    cesoli = '%.2f' %esoli
-    timeupdate = str(r["last_update"])
-
-    update.message.reply_text("Euro Oficial Compra      | " + str(ceoficompra))
-    update.message.reply_text("Euro Oficial Venta       | " + str(ceofiventa))
-    update.message.reply_text("Euro Oficial C/Imp. Pais | " + str(cesoli))
-    update.message.reply_text("Ultima Actualizacion: " + timeupdate[:10] + " | Hora: " + timeupdate[11:19])
-
-
-def euroblue(update, content):
-    r = requests.get('http://api.bluelytics.com.ar/v2/latest').json()
-
-    ebluecompra = float(r["blue_euro"]['value_buy'])
-    cebluecompra = '%.2f' %ebluecompra
-    eblueventa = float(r["blue_euro"]['value_sell'])
-    ceblueventa = '%.2f' %eblueventa
-    timeupdate = str(r["last_update"])
-
-    update.message.reply_text("Euro Blue Compra      | " + str(cebluecompra))
-    update.message.reply_text("Euro Blue Venta       | " + str(ceblueventa))
-    update.message.reply_text("Ultima Actualizacion: " + timeupdate[:10] + " | Hora: " + timeupdate[11:19])
-
-
 def main():
-    """Start the bot."""
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater("1110773023:AAHuxnjeO_XzAc-n_6JZtSRDn4W7FtmoAZ8", use_context=True)
+    updater = Updater(idtoken, use_context=True)
 
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("dolar", dolar))
-    dp.add_handler(CommandHandler("dolarblue", dolarblue))
-    dp.add_handler(CommandHandler("euro", euro))
-    dp.add_handler(CommandHandler("euroblue", euroblue))
-
-    # log all errors
-    dp.add_error_handler(error)
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_error_handler(error)
 
     # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
     updater.idle()
 
 
